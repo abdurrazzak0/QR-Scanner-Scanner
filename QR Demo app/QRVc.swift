e3a786d4a559790bd917495cf5e1a43ef895f25c
abdurrazzak0 <razzak6366@gmail.com>
2023-09-25 14:32:25 +0600

    Initial Commit

---
+++ QR Demo app/QRVc.swift
@@ -0,0 +1,101 @@
+//
+//  QRVc.swift
+//  QR Demo app
+//
+//  Created by Abdur Razzak on 20/9/23.
+//
+
+import UIKit
+import AVFoundation
+
+
+
+class QRVc: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+                                      AVMetadataObject.ObjectType.code39,
+                                      AVMetadataObject.ObjectType.code39Mod43,
+                                      AVMetadataObject.ObjectType.code93,
+                                      AVMetadataObject.ObjectType.code128,
+                                      AVMetadataObject.ObjectType.ean8,
+                                      AVMetadataObject.ObjectType.ean13,
+                                      AVMetadataObject.ObjectType.aztec,
+                                      AVMetadataObject.ObjectType.pdf417,
+                                      AVMetadataObject.ObjectType.itf14,
+                                      AVMetadataObject.ObjectType.dataMatrix,
+                                      AVMetadataObject.ObjectType.interleaved2of5,
+                                      AVMetadataObject.ObjectType.qr]
+    
+    
+    
+    @IBOutlet weak var topBar: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            captureSession.startRunning()
+            view.bringSubviewToFront(messageLabel)
+            view.bringSubviewToFront(topBar)
+            qrCodeFrameView = UIView()
+            
+            if let qrcodeFrameView = qrCodeFrameView {
+                qrcodeFrameView.layer.borderColor = UIColor.yellow.cgColor
+                qrcodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrcodeFrameView)
+                view.bringSubviewToFront(qrcodeFrameView)
+            }
+            
+        } catch {
+            print(error)
+            return
+        }
+    }
+}
+
+extension QRVc: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        // Get the metadata object
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+}
+
+
---
+++ QR Demo app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR Demo app
+//
+//  Created by Abdur Razzak on 20/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
